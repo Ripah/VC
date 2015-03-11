dc802ddfe1ffe4daee7289ee9798ab94fc0779e1
Martin Cristobal Balasch <marti.cristobal@est.fib.upc.edu>
2015-03-11 22:53:36 +0100

    Update 2.2_Entrega_myHisteq.m

--- 2.2_Entrega_myHisteq.m
+++ 2.2_Entrega_myHisteq.m
@@ -1,4 +1,4 @@
-function [  ] = myHisteq( file )
+function [ out ] = myHisteq( file )
     image = imread(file);
     if size(image, 3) == 3
         image = rgb2gray(image);
@@ -6,9 +6,26 @@ function [  ] = myHisteq( file )
     original_hist = imhist(image);
     original_cumsum = cumsum(original_hist);
     
-    figure; plot((original_cumsum/max(original_cumsum))*max(original_hist), 'LineWidth', 2); hold on; bar(original_hist, 'r');
+    figure; 
+    subplot(3,1,1);
+    imshow(image);
+    subplot(3,1,2);
+    imhist(image); 
+    subplot(3,1,3); 
+    plot((original_cumsum/max(original_cumsum))*max(original_hist), 'LineWidth', 2);
     
     
-
+    h = original_hist/numel(image);
+    cdf = cumsum(h);
+    out = 255*cdf(double(image)+1);
+    out = uint8(out);
+    
+    figure;
+    subplot(3,1,1);
+    imshow(out);
+    subplot(3,1,2);
+    imhist(out);
+    subplot(3,1,3);
+    plot((cumsum(imhist(out))/max(cumsum(imhist(out))))*max(imhist(out)), 'LineWidth', 2);
 
 end
