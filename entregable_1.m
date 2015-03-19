a0a023e89ba5755e5bc51da4df0eac5c77e61390
Martin Cristobal Balasch <marti.cristobal@est.fib.upc.edu>
2015-03-20 00:07:46 +0100

    Update entregable_1.m

--- entregable_1.m
+++ entregable_1.m
@@ -12,5 +12,38 @@ for id = 1 : 256
     [~,ind] = min(abs(cdf1(id) - cdf2));
     M(id) = ind-1;
 end
+
 out = M(double(img)+1);
+figure;
+subplot(3,3,1);
+imshow(img);
+title('Original image');
+
+subplot(3,3,2);
+imhist(img);
+title('Original image hist');
+
+subplot(3,3,3);
+plot(cdf1);
+title('Original image cdf');
+
+subplot(3,3,4);
+imshow(out);
+title('Resulting image');
+
+subplot(3,3,5);
+imhist(out);
+title('Resulting image hist');
+
+subplot(3,3,6);
+plot(cumsum(imhist(out))/max(cumsum(imhist(out))));
+title('Resulting image cdf');
+
+subplot(3,3,7);
+plot(hist);
+title('Objective histogram');
+
+subplot(3,3,8);
+plot(M);
+title('Mapping function');
 end
