8a5041830c76cb3f5871cd7ca20c9834ed9b3fc0
Martin Cristobal Balasch <marti.cristobal@est.fib.upc.edu>
2015-04-12 19:42:41 +0200

    Update gaussianFilter.m

--- gaussianFilter.m
+++ gaussianFilter.m
@@ -1,21 +1,21 @@
 function [ out ] = gaussianFilter( sigma_x, sigma_y, size_x, size_y, rotation)
-    % Calcular posiciones
-    X = 0:size_x-1;
-    Y = 0:size_y-1;
-    
+
+    [X,Y] = meshgrid(0:size_x-1, 0:size_y-1);
+    x0 = (size_x-1)/2;
+    y0 = (size_y-1)/2;
     % Rotar las posiciones
+    theta = degtorad(rotation);
+    
+    a = cos(theta)^2/2/sigma_x^2 + sin(theta)^2/2/sigma_y^2;
+    b = -sin(2*theta)/4/sigma_x^2 + sin(2*theta)/4/sigma_y^2 ;
+    c = sin(theta)^2/2/sigma_x^2 + cos(theta)^2/2/sigma_y^2;
+    
+    G = exp( - (a*(X-x0).^2 + 2*b*(X-x0).*(Y-y0) + c*(Y-y0).^2)) ;
     
     
-    % Calcular la gaussiana en 1D para los dos vectores
-    X = (1 / (sigma_x * sqrt(2 * pi))) * exp(- power(X - ((size_x-1)/2), 2) / (2 * power(sigma_x, 2)));
-    Y = (1 / (sigma_y * sqrt(2 * pi))) * exp(- power(Y - ((size_y-1)/2), 2) / (2 * power(sigma_y, 2)));
+    % Normalizar la gaussiana
+    G = G / sum(sum(G));
     
-    % Normalizar los vectores
-    X = X / sum(X);
-    Y = Y / sum(Y);
-    
-    % Calcular la matriz
-    M = Y.' * X;
-    
-    out = M;
+    %surf(X,Y,G);shading interp;view(-36,36)
+    out = G;
 end
